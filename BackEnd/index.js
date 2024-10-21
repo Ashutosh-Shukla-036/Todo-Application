1b22b6f901876f30e9b660a2f4423ace960aaa7e
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-10-21 21:27:40 +0530

    Final Backend

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -7,7 +7,8 @@ const app = express()
 
 app.use(express.json());
 app.use(cors({
-    origin: 'https://todoapplication-five.vercel.app/'
+    origin: 'https://todoapplication-five.vercel.app', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
 }));
 connectDB();
 
