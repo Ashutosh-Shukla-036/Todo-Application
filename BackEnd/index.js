61852dcdf1d53ba1d5e04422936f061dfe7d2a08
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-10-21 20:52:11 +0530

    cors origin

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -6,7 +6,9 @@ const task = require('./routes/taskRoute')
 const app = express()
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://your-frontend-url.vercel.app'
+}));
 connectDB();
 
 app.get('/',(req,res) => {
